351d5e3f389d0ea3a3e719a60ac14597205d84de
Jamie STevens <jamiestevens2000@gmail.com>
2021-04-20 16:28:48 +0100

    Added poetry support

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Detection-of-Manipulated-Pricing-in-Smart-Energy-CPS-Scheduling
+Ensure you have Python and Poetry installed. 
+
+First install dependencies with:
+```
+poetry install
+```
+
+Then run with:
+```
+poetry run python main.py
+```
--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from sklearn.model_selection import train_test_split
 import pandas
 
 #Load training data
-# n/a stands for normal/abnormal 
+#n/a stands for normal/abnormal 
 trainingData = pandas.read_csv('TrainingData.txt', names=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,"n/a"])
 data = trainingData.iloc[:,0:24]
 target = trainingData.loc[:,"n/a"].values
 
-##Split training data
+#Split training data
 X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3,random_state=13) # 70% training and 30% test
 
 #Create a svm Classifier
@@ -32,4 +32,5 @@ test_Pred = clf.predict(testingData)
 
 #Append labels and save to file 
 testingData["n/a"] = test_Pred
+print(testingData)
 testingData.to_csv("TestingResults.txt", header=False, index=False)
